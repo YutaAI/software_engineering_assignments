e216c8b85fc69c2892b211b74beb5a1e82096268
soumithvallapalli <soumithvallapalli@gmail.com>
2026-05-02 23:51:18 -0400

    Delete old Final Project folder

--- Final Project/api/routers/index.py
+++
@@ -1,9 +0,0 @@
-from . import orders, order_details, recipes, sandwiches, resources
-
-
-def load_routes(app):
-    app.include_router(orders.router)
-    app.include_router(order_details.router)
-    app.include_router(sandwiches.router)
-    app.include_router(recipes.router)
-    app.include_router(resources.router)
